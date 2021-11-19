ea97ee9c9335da0feb6ba692adb43841df85282b
Cloud Worm <70105976+cloud-worm@users.noreply.github.com>
2021-11-19 20:04:13 +0100

    Add files via upload

---
+++ sea.h
@@ -0,0 +1,25 @@
+#include <iso646.h>
+
+#define if       if (
+#define then     ) {
+#define end      }
+#define gives    {
+#define is       ==
+#define start    int main(int argc, char const *argv[]) {
+#define run 
+#define use      using namespace
+#define sub      ::
+#define pt       *
+#define ref      &
+#define to       =
+#define point    *
+#define out      <<
+#define in       >>
+#define for      for (
+#define while    while (
+#define incr     ++
+#define decr     --
+#define __       ;
+#define pt       *
+#define dealloc  delete
+#define import   #include
